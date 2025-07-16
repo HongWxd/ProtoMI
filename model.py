c90eb07ade2f6195190e294c2491eeeb6ced3e4d
Hong Weixiang <1070446604@qq.com>
2025-07-16 09:33:13 +0800

    update

--- model.py
+++ model.py
@@ -145,6 +145,7 @@ class GINE_descriptor(torch.nn.Module):
         x = self.conv3(x, edge_index, edge_attr)
         x = F.relu(x)
         x = self.dropout(x)
+        print(x.shape)
 
         x = global_mean_pool(x, batch)# [batchsize, hidden_channels]
 
